5735c5db481decbc729e29534c1ea778bf0b5ecc
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-10-31 22:05:10 -0300

    Add files via upload

---
+++ att1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace att1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[100];
+            Random r = new Random();
+
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                vetor[i] = r.Next(0, 101);
+                
+            }
+
+            Console.WriteLine("De um valor de 0  a 100 para procurar:");
+            int busc = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O valor esta no indice {Busca(vetor, busc)}");
+
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.Write(vetor[i] + " ");
+
+            }
+
+            Console.ReadLine();
+        }
+
+
+        static int Busca(int[] array, int x)
+        {
+            int resp = -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == x)
+                {
+                    resp = i;
+                    return i;
+                }
+
+            }
+
+            return resp;
+        }
+
+
+      
+    }
+}
---
+++ att2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace att2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] vetor = new int[10];
+            Random r = new Random();
+
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                vetor[i] = i;
+
+            }
+
+            Console.WriteLine("De um valor de 0  a 10 para procurar:");
+            int busc = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O valor esta no indice {Pesquisa(vetor, busc)}");
+
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.Write(vetor[i] + " ");
+
+            }
+
+            Console.ReadLine();
+
+        }
+
+
+        static int Pesquisa(int[] array, int x) 
+        {
+            int resp = -1;
+          
+            int dir = array.Length - 1, esq = 0, meio;
+            while (esq <= dir)
+            {
+                meio = (esq + dir) / 2;
+                if (x == array[meio])
+                {
+                    resp = meio;
+                    esq = array.Length - 1;
+                }
+                else if (x > array[meio])
+                {
+                    esq = meio + 1;
+                }
+                else
+                {
+                    dir = meio - 1;
+                }
+            }
+
+            return resp;
+        }
+    }
+
+
+}
